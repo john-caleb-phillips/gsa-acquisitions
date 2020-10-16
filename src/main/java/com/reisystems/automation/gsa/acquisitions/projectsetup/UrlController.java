083f14923effd67aa4fabb3882989529d878c9f0
John Phillips <john.phillips@reisystems.com>
2020-10-16 10:16:27 -0400

    Minor changes

--- src/main/java/com/reisystems/automation/gsa/acquisitions/projectsetup/UrlController.java
+++ src/main/java/com/reisystems/automation/gsa/acquisitions/projectsetup/UrlController.java
@@ -4,11 +4,6 @@ import com.reisystems.blaze.controller.EnvironmentController;
 
 public class UrlController {
     public static String getPortalUrl() {
-        return switch (EnvironmentController.getEnvironmentName()) {
-            case "QA" -> "https://qa.digitaldashboard.gov/";
-            case "UAT" -> "https://uat.digitaldashboard.gov/";
-            case "PROD" -> "https://www.digitaldashboard.gov/";
-            default -> "invalid environment";
-        };
+        return "https://www.acquisition.gov/";
     }
 }
